0b0c1406eccc6ce13580c399af5cdda4497ad7f4
Harun <149244335+harunzsln@users.noreply.github.com>
2025-07-21 11:44:03 +0300

    Update server.js

--- contact-backend/server.js
+++ contact-backend/server.js
@@ -33,17 +33,17 @@ app.post('/contact', async (req, res) => {
             subject: `New contact form submission from ${name}`,
             text: "from =" + email + " " + message,*/
 
-            from: `"${name}" <${process.env.SMTP_USER}>`,  // Gönderen senin adresin
-            to: process.env.TO_EMAIL,                     // Alıcı sensin
-            replyTo: email,                               // Cevapla dendiğinde kullanıcıya gider
+            from: `"${name}" <${process.env.SMTP_USER}>`, 
+            to: process.env.TO_EMAIL,                     
+            replyTo: email,                              
             subject: `Yeni mesaj: ${name}`,
             text: `
 Yeni bir mesaj aldınız:
 
-👤 İsim: ${name}
-📧 E-posta: ${email}
-
-📝 Mesaj:
+  İsim: ${name}
+  E-posta: ${email}
+ 
+  Mesaj:
 ${message}
   `,
 
